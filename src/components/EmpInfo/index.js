6e2f34ecd0c24fd4ec1203ceb22d95afaed8f975
Taylor Bruno <taylorbruno895@gmail.com>
2020-03-06 21:39:16 -0500

    can not sort in one order by each column

--- src/components/EmpInfo/index.js
+++ src/components/EmpInfo/index.js
@@ -4,6 +4,8 @@ import Container from "../Container";
 import employees from "../../utils/employees";
 import API from "../../utils/API";
 
+let order = false;
+
 class Table extends React.Component {
     constructor(props){
         super(props)
@@ -13,6 +15,72 @@ class Table extends React.Component {
             results: [],
             error: ""
         };
+    }
+
+    sortName = async (event) => {
+        event.preventDefault();
+        const col = event.target.dataset.value;
+        console.log("sorting by \n-----\n", col ,"\n--------\n ");
+        // order = !order;
+        if (order === false) {
+            // order = "asc";
+            const sorted = await [].concat(this.state.employees)
+                .sort(function (a, b) {
+                    let keyToSort;
+                    let x;
+                    let y;
+                    switch (col) {
+                        case "name": 
+                            keyToSort = [a.name, b.name]; 
+                            x = keyToSort[0].toLowerCase();
+                            y = keyToSort[1].toLowerCase();
+                            break;
+                        case "email": 
+                            keyToSort = [a.email, b.email]; 
+                            x = keyToSort[0].toLowerCase();
+                            y = keyToSort[1].toLowerCase();
+                            break;
+                        case "phone": 
+                            keyToSort = [a.phone, b.phone]; 
+                            x = keyToSort[0].toLowerCase();
+                            y = keyToSort[1].toLowerCase();
+                            break;
+                        case "birth": 
+                            keyToSort = [a.dateOfBirth, b.dateOfBirth]; 
+                            x = keyToSort[0];
+                            y = keyToSort[1];
+                            break;
+                        default: keyToSort = [a.name, b.name]; break;
+                    }
+                    
+                    order =!order;
+                    console.log(`${x} vs ${y}`);
+
+                    switch (order) {
+                        case true: 
+                            if(x < y) return -1;
+                            else if(x > y) return 1;
+                            else return 0;
+                        case false:
+                            if(x < y) return -1;
+                            else if(x > y) return 1;
+                            else return 0;
+                        default: 
+                            if(x < y) return -1;
+                            else if(x > y) return 1;
+                            else return 0;
+                    }
+                    
+                    
+                }
+            );
+            console.log("---sorted---", sorted);
+            this.setState({ 
+                ...this.state,
+                employees: sorted 
+            });
+        }
+        // else if (order === "")
     }
 
     handleChange = (event) => {
@@ -60,14 +128,30 @@ class Table extends React.Component {
                     <input type="text" className="form-control searchBar" placeholder="Search" onChange={this.handleChange} /> 
                 </div>
                 <div className="table-responsive">
-                    <table className="table">
+                    <table className="table table-striped table-bordered">
                         <thead key="thead">
                             <tr key="theadTR">
                                 <th>Image</th>
-                                <th>Name</th>
-                                <th>Phone Number</th>
-                                <th>Email</th>
-                                <th>Date of Birth</th>
+                                <th>Name 
+                                    <button className="button sortButton" onClick={this.sortName}>
+                                        <i className="fas fa-sort" data-value="name"></i>
+                                    </button>
+                                </th>
+                                <th>Phone Number
+                                    <button className="button sortButton" onClick={this.sortName}>
+                                        <i className="fas fa-sort" data-value="phone"></i>
+                                    </button>
+                                </th>
+                                <th>Email
+                                    <button className="button sortButton" onClick={this.sortName}>
+                                        <i className="fas fa-sort" data-value="email"></i>
+                                    </button>
+                                </th>
+                                <th>Date of Birth
+                                    <button className="button sortButton" onClick={this.sortName}>
+                                        <i className="fas fa-sort" data-value="birth"></i>
+                                    </button>
+                                </th>
                             </tr>
                         </thead>
                         <tbody key="tbody">
