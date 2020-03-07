0e3f8d82085f780747baef57a48adb1b557c82ab
Taylor Bruno <taylorbruno895@gmail.com>
2020-03-07 09:34:44 -0500

    adding build files for heroku deployment

---
+++ build/precache-manifest.027192323e925842d6133dcc51dffefd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef0cb0e87481848fd20a6d8a6a6833e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b4a9ee48942b6d8ef71",
+    "url": "/static/css/main.bd44324b.chunk.css"
+  },
+  {
+    "revision": "8d6ec57a45865c82c574",
+    "url": "/static/js/2.c899d7ab.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.c899d7ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4a9ee48942b6d8ef71",
+    "url": "/static/js/main.448c63fc.chunk.js"
+  },
+  {
+    "revision": "5e8575281faecd369db7",
+    "url": "/static/js/runtime-main.25a038dd.js"
+  }
+]);
